2c461cf4bba1cf35233d24b861a1030b40ef3232
xushanpu123 <798304030@qq.com>
2024-05-06 14:27:05 +0800

    finish the ch2

--- user/src/bin/04priv_csr.rs
+++ user/src/bin/04priv_csr.rs
@@ -4,14 +4,14 @@
 #[macro_use]
 extern crate user_lib;
 
-use riscv::register::sstatus::{self, SPP};
+//use riscv::register::sstatus::{self, SPP};
 
 #[no_mangle]
 fn main() -> i32 {
     println!("Try to access privileged CSR in U Mode");
     println!("Kernel should kill this application!");
-    unsafe {
-        sstatus::set_spp(SPP::User);
-    }
+    //unsafe {
+        //sstatus::set_spp(SPP::User);
+   // }
     0
 }
