3c2d70c7cb2d1b83eddb5e9487df33c8a846ffb2
Huzhiwen1208 <Huzhiwen1208@users.noreply.github.com>
2024-05-10 02:21:55 +0000

    deploy: e188c2d3896268cb16ba7e67e0f695aa10441d59

--- ch9/os/mm/page_table/sidebar-items.js
+++ ch9/os/mm/page_table/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["translated_byte_buffer","translated_ref","translated_refmut","translated_str"],"struct":["PTEFlags","PageTable","PageTableEntry","UserBuffer","UserBufferIterator"]};
+window.SIDEBAR_ITEMS = {"fn":["str_len","translated_byte_buffer","translated_ref","translated_refmut","translated_str"]};
